92bd775bbdd5b35bdbe88cac383fadba8f7041d1
Eli <emmuzio0510@icloud.com>
2023-11-07 16:22:34 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningGame = record.find(game => game.result === 'W');
+  
+    if (winningGame) {
+      return winningGame.year;
+    } else {
+      return undefined; 
+    }
+  }
+  
+  module.exports = superbowlWin;
+  
